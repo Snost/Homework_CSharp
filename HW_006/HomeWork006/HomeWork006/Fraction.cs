74f0e677802b4a5be3651145b790ad2122887c2c
Snost <snizhana.lala@gmail.com>
2024-04-28 21:47:08 +0300

    hw006

---
+++ HW_006/HomeWork006/HomeWork006/Fraction.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HomeWork006
+{
+    class Fraction
+    {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        if (denominator != 0)
+            this.denominator = denominator;
+        else
+            throw new ArgumentException("Denominator cannot be 0.");
+
+        Calculate();
+    }
+
+    private void Calculate()
+    {
+        int divisor = Divisor(Math.Abs(numerator), Math.Abs(denominator));
+        numerator /= divisor;
+        denominator /= divisor;
+        if (denominator < 0)
+        {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+    }
+
+    private int Divisor(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static Fraction operator +(Fraction a, Fraction b)
+    {
+        return new Fraction(a.numerator * b.denominator + b.numerator * a.denominator, a.denominator * b.denominator);
+    }
+
+    public static Fraction operator -(Fraction a, Fraction b)
+    {
+        return new Fraction(a.numerator * b.denominator - b.numerator * a.denominator, a.denominator * b.denominator);
+    }
+
+    public static Fraction operator *(Fraction a, Fraction b)
+    {
+        return new Fraction(a.numerator * b.numerator, a.denominator * b.denominator);
+    }
+
+    public static Fraction operator /(Fraction a, Fraction b)
+    {
+        return new Fraction(a.numerator * b.denominator, a.denominator * b.numerator);
+    }
+
+    public static bool operator ==(Fraction a, Fraction b)
+    {
+        return a.numerator == b.numerator && a.denominator == b.denominator;
+    }
+
+    public static bool operator !=(Fraction a, Fraction b)
+    {
+        return !(a == b);
+    }
+
+    public static bool operator >(Fraction a, Fraction b)
+    {
+        return (double)a.numerator / a.denominator > (double)b.numerator / b.denominator;
+    }
+
+    public static bool operator <(Fraction a, Fraction b)
+    {
+        return (double)a.numerator / a.denominator < (double)b.numerator / b.denominator;
+    }
+
+    public static Fraction operator ++(Fraction a)
+    {
+        return new Fraction(a.numerator + a.denominator, a.denominator);
+    }
+
+    public static Fraction operator --(Fraction a)
+    {
+        return new Fraction(a.numerator - a.denominator, a.denominator);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || !(obj is Fraction))
+            return false;
+
+        Fraction equal = (Fraction)obj;
+        return this == equal;
+    }
+
+    public override int GetHashCode()
+    {
+        return numerator.GetHashCode() ^ denominator.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+
+    
+}
+}
---
+++ HW_006/HomeWork006/HomeWork006/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace HomeWork006
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fract1 = new Fraction(3, 4);
+            Fraction fract2 = new Fraction(2, 5);
+
+            Console.WriteLine(fract1);
+            Console.WriteLine(fract2);
+
+            Fraction sum = fract1 + fract2;
+            Console.WriteLine($"Sum: {sum}");
+
+            Fraction difference = fract1 - fract2;
+            Console.WriteLine($"Difference: {difference}");
+
+            Fraction product = fract1 * fract2;
+            Console.WriteLine($"Product: {product}");
+
+            Fraction quotient = fract1 / fract2;
+            Console.WriteLine($"Quotient: {quotient}");
+
+            Console.WriteLine($"fract1 == fract2: {fract1 == fract2}");
+
+            Console.WriteLine($"fract1 != fract2: {fract1 != fract2}");
+
+            Console.WriteLine($"fract1 > fract2: {fract1 > fract2}");
+
+            Console.WriteLine($"fract1 < fract2: {fract1 < fract2}");
+
+            Fraction f3 = fract1++;
+            Console.WriteLine($"fraction++: {f3}");
+
+            Fraction f4 = --fract2;
+            Console.WriteLine($"--fraction: {f4}");
+
+            
+        }
+    }
+    }
+
+
+
+
+
+
