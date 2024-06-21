07c1699c105cd4d542f1a8fbd8eceba4742ea763
Snost <snizhana.lala@gmail.com>
2024-06-21 21:43:15 +0300

    HW done

---
+++ HW_WPF_003/HW_WPF_003/HW_WPF_003/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_WPF_003
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Border).Background = Brushes.DarkRed;
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Border).Background = Brushes.DarkRed;
+        }
+    }
+}
